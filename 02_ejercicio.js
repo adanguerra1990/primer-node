bbcbd3245fb00e93fef7bab562e88bebfe129884
adanguerra1990 <aj90guerra@gmail.com>
2022-10-19 16:55:35 -0400

    Ejercio de sintaxi variables y palabras reservadas

--- 02_ejercicio.js
+++ 02_ejercicio.js
@@ -1,19 +1,16 @@
-const nombre = "Adan Guerra";
-const edad = 32;
-const esDesarrollador = true;
-const fechaNacimiento = new Date('Julio 16, 1990');
-const libroFavorito = {
-    titulo: "Mi Jardin",
-    autor: "Autor",
-    fecha: "15/05/1995",
-    url: "https://www.libros.com"
-}
+const lista = [
+    {
+        nombre: "Adan Guerra",
+        edad: 32,
+        esDesarrollador: true,
+        fechaNacimiento: new Date('Julio 16, 1990')
+    },
+    {
+        titulo: "Mi Jardin",
+        autor: "Autor",
+        fecha: "15/05/1995",
+        url: "https://www.libros.com"
+    }
+];
 
-
-const myBios = {
-    nombre: "Adan Guerra",
-    edad: 32,
-    esDesarrollador: true,
-    fechaNacimiento: new Date('Julio 16, 1990')
-}
-console.log(myBios, libroFavorito)
+console.log(lista);
