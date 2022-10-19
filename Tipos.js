4935729284820c466ecd4a0374bf2d67056ec803
adanguerra1990 <aj90guerra@gmail.com>
2022-10-19 16:44:49 -0400

    Ejercio de sintaxi variables y palabras reservadas

---
+++ Tipos.js
@@ -0,0 +1,27 @@
+// Tipos Primitivos
+
+// number
+4;
+0;
+
+// String
+"hola";
+'hola';
+`hola`;
+
+//Boolenos
+true;
+false;
+
+// nulo y undefined
+null;
+undefined;
+
+// null, undefined, false, 0 ==> Todas son falsy
+console.log(null === undefined);
+
+if (false) {
+    console.log("cumple")
+} else {
+    console.log("no cumple")
+}
